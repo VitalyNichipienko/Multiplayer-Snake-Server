c9099a62934545f037fcd5829e767a07a7402c69
VitalyNichipienko <nichipienkova@outlook.com>
2024-04-13 00:20:08 +0700

    Implemented apple collecting

--- src/rooms/02-state-handler.ts
+++ src/rooms/02-state-handler.ts
@@ -2,25 +2,45 @@ import { Room, Client } from "colyseus";
 import { Schema, type, MapSchema, ArraySchema } from "@colyseus/schema";
 
 export class Vector2Float extends Schema{
-    @type("number")x = Math.floor(Math.random() * 128) -64;
+    @type("uint32") id = 0;
+    @type("number") x = Math.floor(Math.random() * 128) -64;
     @type("number") z = Math.floor(Math.random() * 128) -64;
 }
 
 export class Player extends Schema {
     @type("number") x = Math.floor(Math.random() * 128) -64;
     @type("number") z = Math.floor(Math.random() * 128) -64;
-    @type("uint8") detailCount = 2; 
+    @type("uint8") detailCount = 0; 
     @type("uint8") skinIndex = 0;
+    @type("uint16") score = 0;
 }
 
 export class State extends Schema {
     @type({ map: Player }) players = new MapSchema<Player>();    
     @type([Vector2Float]) apples = new ArraySchema<Vector2Float>();
 
+    appleLastId = 0;
+
     colorIndices: number[] = [0, 1, 2, 3, 4, 5, 6, 7];
 
     createApple(){
-        this.apples.push(new Vector2Float());
+        const apple = new Vector2Float();
+        apple.id = this.appleLastId;
+        this.apples.push(apple);
+        this.appleLastId++;
+    }
+
+    collectApple(player: Player, data: any){
+        const apple = this.apples.find((value) => value.id === data.id);
+
+        if (apple === undefined)
+            return;
+        
+        apple.x = Math.floor(Math.random() * 128) -64;
+        apple.z = Math.floor(Math.random() * 128) -64;
+
+        player.score++;
+        player.detailCount = Math.round(player.score / 3);
     }
 
     createPlayer(sessionId: string) {
@@ -39,7 +59,7 @@ export class State extends Schema {
         this.players.get(sessionId).x = movement.x;
         this.players.get(sessionId).z = movement.z;
     }
-    
+
     private getRandomColorIndexAndRemove(): number {
         const randomIndex = Math.floor(Math.random() * this.colorIndices.length);
         return this.colorIndices.splice(randomIndex, 1)[0];
@@ -57,6 +77,11 @@ export class StateHandlerRoom extends Room<State> {
 
         this.onMessage("move", (client, data) => {
             this.state.movePlayer(client.sessionId, data);
+        });
+
+        this.onMessage("collect", (client, data) => {
+            const player = this.state.players.get(client.sessionId);
+            this.state.collectApple(player, data);
         });
 
         for (let i = 0; i < this.startAppleCount; i++)
